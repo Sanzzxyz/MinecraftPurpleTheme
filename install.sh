78f5aa57b8fb517674f86c85673907980d085aa0
Sanzzxyz <133630867+Sanzzxyz@users.noreply.github.com>
2023-05-15 14:18:31 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,7 +47,7 @@ installThemeQuestion(){
 }
 
 repair(){
-    bash <(curl https://raw.githubusercontent.com/Iyanbot/MinecraftPurpleTheme/main/repair.sh)
+    bash <(curl https://raw.githubusercontent.com/Sanzzxyz/MinecraftPurpleTheme/main/repair.sh)
 }
 
 restoreBackUp(){
